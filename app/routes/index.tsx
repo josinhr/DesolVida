a64ebe1cedeef1845ab84ce587f654b571c47a5f
jhernan6 <jose.hernandez-rizo@capgemini.com>
2021-12-22 08:56:17 +0100

    add css to title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,23 @@
+import { LinksFunction } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
 export default function Index() {
   return (
-    <div >
-      <h1 >Hello world</h1>
+    <div>
+      <header>
+        <div className="mainTitle">
+          <h1>DesolVida</h1>
+          <img
+            src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/brain_1f9e0.png"
+            alt="brain image"
+          />
+        </div>
+      </header>
+      <div></div>
     </div>
   );
 }
