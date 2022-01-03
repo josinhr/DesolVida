0e57f32feebcca9dd942409b2e379d25ee8a551c
josinhr <josinhr@gmail.com>
2022-01-03 17:12:56 +0100

    now can scroll with mouse

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,27 @@
 import { Link, LinksFunction, useLoaderData, LoaderFunction } from "remix";
 import stylesUrl from "../styles/index.css";
 import { indexImages } from "~/assets/indexImages";
+import React, { useRef, useEffect } from "react";
+
+export function useHorizontalScroll() {
+  const elRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  useEffect(() => {
+    const el: any = elRef.current;
+    if (el) {
+      const onWheel = (e: { deltaY: number; preventDefault: () => void }) => {
+        if (e.deltaY == 0) return;
+        e.preventDefault();
+        el.scrollTo({
+          left: el.scrollLeft + e.deltaY,
+          behavior: "smooth",
+        });
+      };
+      el.addEventListener("wheel", onWheel);
+      return () => el.removeEventListener("wheel", onWheel);
+    }
+  }, []);
+  return elRef;
+}
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -12,7 +33,7 @@ export let loader: LoaderFunction = async () => {
 
 export default function Index() {
   const data = useLoaderData<number[]>();
-  console.log("This is data" + data);
+  const scrollRef = useHorizontalScroll();
 
   return (
     <div>
@@ -32,9 +53,17 @@ export default function Index() {
         </Link>
       </header>
       <div>
-        {data.map((n) => (
-          <img id={n.toString()} src={indexImages[n - 1]} alt="Old women" />
-        ))}
+        <div ref={scrollRef} id="photoCarrousel" className="photoCarrousel">
+          {data.map((n) => (
+            <img id={n.toString()} src={indexImages[n - 1]} alt="Old women" />
+          ))}
+        </div>
+        <button type="button" className="button">
+          {"<"}
+        </button>
+        <button type="button" className="button">
+          {">"}
+        </button>
       </div>
     </div>
   );
